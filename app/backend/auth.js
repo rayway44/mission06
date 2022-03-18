1402c2d563e08d5da0aabe871f5e0024a5ae187e
Stuart Toleafoa <stuarttoley@gmail.com>
2022-03-18 15:28:43 +1300

    backend again

--- app/backend/auth.js
+++ app/backend/auth.js
@@ -6,45 +6,18 @@ const dataModel = require('./schema')
 //all listings
 router.get('/data', async (req, res) => {
     try {
-        // const regex = /https:\/\/.*giphy.*\/media\/\w+\/giphy.gif\?cid=[a-f0-9]+&rid=giphy\.gif&ct=g/gi
-       const products = await dataModel.find()
-        res.send(products)
-        console.log(products)
-    } catch (err) {
-        res.json({message: err.message})
-    }
-})
-
-
-router.get('/data/:query', async (req, res) => {
-    try {
-
-       const query =req.params.query
-       console.log(query)
-       const products = await Products.find({suburbs:{suburbs}})
-        res.send(products)
-        console.log(products)
-
-    } catch (err) {
-        res.json({message: err.message})
-    }
-})
-
-router.get('/datas/:query', (req, res) => {
-
-       const query =req.params.query
-       console.log(query)
-    //    const products = Products.find({suburbs:{suburbs}, bedrooms:{bedrooms}, bathrooms:{bathrooms}})
-        // res.send(products)
-        // console.log(products)
-        
     
+        const products = await dataModel.find()
+        res.send(products)
+        console.log(products)
+        
+    } catch (err) {
+        res.json({message: err.message})
+    }
 })
 
 
-router.get('/test', (req, res) => {
-    res.send('testing')
-})
+
 
 
 module.exports = router
