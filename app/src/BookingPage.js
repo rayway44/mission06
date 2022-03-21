bd76aa843203d1717976217e553dbaf077ce8551
Ray Letoa <rayletoa@RAYway-2.local>
2022-03-22 00:31:17 +1300

    images for ammenities done

--- app/src/BookingPage.js
+++ app/src/BookingPage.js
@@ -3,6 +3,7 @@ import BookingPageNavBar from './Components/BookingPageNavBar'
 import BookingPageMainImg from './Components/BookingPageMainImg'
 import BookingMainPageSectionOne from './Components/BookingMainPageSectionOne'
 import BookingPageFooter from './Components/BookingPageFooter'
+import BookingMainPageSectionTwo from './Components/BookingMainPageSectionTwo'
 import './Components/BookingPage.css'
 
 export default function BookingPage() {
@@ -11,6 +12,7 @@ export default function BookingPage() {
         <BookingPageNavBar />
         <BookingPageMainImg />
         <BookingMainPageSectionOne />
+        <BookingMainPageSectionTwo />
         <BookingMainPageSectionOne />
         <BookingPageFooter />
 
