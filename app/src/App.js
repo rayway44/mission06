a69e9c59396a2e2ebc42dc5783863ddc437e7ab5
Stuart Toleafoa <stuarttoley@gmail.com>
2022-03-22 21:00:30 +1300

    listing page

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,16 @@
 // import Listing from './components/Listing/Listing';
 import './App.css';
-import BookingPage from './BookingPage'
+// import BookingPage from './BookingPage'
+import ListingPage from './ListingPage';
 // import CardCarousel from './components/CardCarousel/CardCarousel';
 // import FormSection from './components/FormSection/FormSection';
 
 function App() {
   return (
     <div class='App'>
-      <BookingPage /> 
+      {/* <BookingPage />  */}
       {/* <LandingPage /> */}
+      <ListingPage />
     </div>
   );
 }
---
+++ app/src/ListingPage.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import BookingPageNavBar from './Components/BookingPageNavBar'
+import Listing from './Components/Listing/Listing'
+import Map from './Components/Map'
+
+import './ListingPage.css'
+
+import { useLoadScript } from "@react-google-maps/api";
+
+
+function ListingPage() {
+
+
+  const API_KEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY
+    const { isLoaded } = useLoadScript({
+      googleMapsApiKey: API_KEY // Add your API key
+    });
+
+    if(!isLoaded) return <div>Hold the caller. . .</div>
+
+  return (
+    <div className='listing-page-border-patrol'>
+        <BookingPageNavBar />
+        <div className='listing-page-complied-container'>
+          <Listing />
+          
+          <div className='listing-page-map-holder'>
+            <Map />
+          </div>
+        
+        </div>
+        
+    </div>
+  )
+}
+
+export default ListingPage
